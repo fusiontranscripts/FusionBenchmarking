8a04ddb2a95e859dfc4bedb980f76067ae0de7d5
brianjohnhaas <brianjohnhaas@gmail.com>
2019-04-24 14:19:38 +0000

    added F1 plotter

---
+++ util/plot_F1_vs_min_frags.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+options(stringsAsFactors = FALSE)
+
+args<-commandArgs(TRUE)
+
+if (length(args) == 0) {
+    stop("require param: min_X.results.scored.ROC")
+}	
+
+roc_file = args[1]
+
+library(tidyverse)
+
+data = read.table(roc_file, header=T)
+
+p = data %>% filter(min_sum_frags <= 20) %>% ggplot(aes(x=min_sum_frags, y=F1, color=prog)) + geom_line()
+
+pdf_filename = paste0(roc_file, "F1_vs_minFrags.pdf")
+pdf(pdf_filename, width=8)
+
+plot(p)
+
+
+
