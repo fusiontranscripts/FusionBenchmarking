9642f9739057be120e81273ca968a52b598b8eec
brianjohnhaas <brianjohnhaas@gmail.com>
2019-04-25 16:57:37 +0000

    update

--- cancer_cell_lines/Edgren_subset/runMe.sh
+++ cancer_cell_lines/Edgren_subset/runMe.sh
@@ -2,6 +2,9 @@
 
 set -ev
 
+
+cat ../preds.collected.gencode_mapped.wAnnot.filt | egrep '^(sample|BT474|MCF7|KPL4|SKBR3)' > preds.collected.gencode_mapped.wAnnot.filt.edgren
+
 ./analyze_Edgren_subset.pl
 
 ../../benchmarking/Venn_analysis_strategy.pl preds.collected.gencode_mapped.wAnnot.filt.edgren ../progs_select.txt
