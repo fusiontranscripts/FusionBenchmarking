4236dc840c92f35a71d9afef1966da8287ddbf23
Brian Haas <brianjohnhaas@gmail.com>
2019-05-08 11:29:32 -0400

    adding upsetr plot

---
+++ benchmarking/plotters/plot_upsetR.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+options(stringsAsFactors = FALSE)
+
+args<-commandArgs(TRUE)
+
+if (length(args) == 0) {
+    stop("require param: matrix")
+}
+
+prog_agree_matrix = args[1]
+
+library(tidyverse)
+library(UpSetR)
+
+
+pdf_filename = sprintf("%s.UpSetR.pdf", prog_agree_matrix)
+pdf(pdf_filename, width=11)
+
+data = read.table(prog_agree_matrix, header=T)
+
+upset(data, number.angles=90, nsets=1000, nintersects=1000)
+
+dev.off()
+
+
+
+
--- cancer_cell_lines/Edgren_subset/runMe.sh
+++ cancer_cell_lines/Edgren_subset/runMe.sh
@@ -5,6 +5,14 @@ set -ev
 
 cat ../preds.collected.gencode_mapped.wAnnot.filt | egrep '^(sample|BT474|MCF7|KPL4|SKBR3)' > preds.collected.gencode_mapped.wAnnot.filt.edgren
 
+
+## examine min3 agree Venn
+./eval_edgren_min_agree.pl  preds.collected.gencode_mapped.wAnnot.filt.edgren 3  > edgren.min3
+
+../../benchmarking/plotters/plot_upsetR.R edgren.min3
+
+
+## analyze accuracy
 ./analyze_Edgren_subset.pl
 
 ./examine_validated_enrichment.R edgren.truthset preds.collected.gencode_mapped.wAnnot.filt.edgren.byProgAgree preds.collected.gencode_mapped.wAnnot.filt.edgren
