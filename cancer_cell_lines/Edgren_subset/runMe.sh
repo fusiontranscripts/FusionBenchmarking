001be9b864d2a62e5af65f90fa34ab4de4e8e261
Brian Haas <brianjohnhaas@gmail.com>
2019-05-13 15:55:06 -0400

    consider paralogs and alt mappings in enrichment analysis

--- cancer_cell_lines/Edgren_subset/runMe.sh
+++ cancer_cell_lines/Edgren_subset/runMe.sh
@@ -6,8 +6,6 @@ set -ev
 cat ../preds.collected.gencode_mapped.wAnnot.filt | egrep '^(sample|BT474|MCF7|KPL4|SKBR3)' > preds.collected.gencode_mapped.wAnnot.filt.edgren
 
 
-## examine min3 agree Venn
-./eval_edgren_min_agree.pl  preds.collected.gencode_mapped.wAnnot.filt.edgren 3  > edgren.min3
 
 ../../benchmarking/plotters/plot_upsetR.R edgren.min3
 
@@ -15,7 +13,13 @@ cat ../preds.collected.gencode_mapped.wAnnot.filt | egrep '^(sample|BT474|MCF7|K
 ## analyze accuracy
 ./analyze_Edgren_subset.pl
 
-./examine_validated_enrichment.R edgren.truthset preds.collected.gencode_mapped.wAnnot.filt.edgren.byProgAgree preds.collected.gencode_mapped.wAnnot.filt.edgren
+
+## examine enrichment for valid fusions among minProgs
+./examine_validated_enrichment.R edgren.truthset.raw preds.collected.gencode_mapped.wAnnot.filt.edgren.scored
+
+
+## examine min3 agree Venn
+./eval_edgren_min_agree.pl  preds.collected.gencode_mapped.wAnnot.filt.edgren 3  > edgren.min3
 
 
 ../../benchmarking/Venn_analysis_strategy.pl preds.collected.gencode_mapped.wAnnot.filt.edgren ../progs_select.txt
